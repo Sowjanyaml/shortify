db6f06b3480941590be77fdf5cc9e0accce4f51d
lakshmi <sowjanya.ml@gmail.com>
2023-03-19 20:15:36 +0100

    More test cases

--- src/test/java/com/example/shortify/service/ShortifyServiceTest.java
+++ src/test/java/com/example/shortify/service/ShortifyServiceTest.java
@@ -1,6 +1,8 @@
 package com.example.shortify.service;
 
+import com.example.shortify.dto.LongUrlDto;
 import com.example.shortify.entity.ShortUrl;
+import com.example.shortify.exception.UrlInvalidException;
 import com.example.shortify.exception.UrlNotFoundException;
 import com.example.shortify.repository.ShortifyRepository;
 import org.assertj.core.api.Assertions;
@@ -9,12 +11,15 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.util.ReflectionTestUtils;
 
-
+import java.time.LocalDateTime;
 import java.util.Optional;
 
-import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class ShortifyServiceTest {
@@ -41,5 +46,30 @@ public class ShortifyServiceTest {
                 .thenReturn(Optional.empty());
         Assertions.assertThatThrownBy(() -> shortifyService.getUrlByKey("abc")).isInstanceOf(UrlNotFoundException.class);
     }
+
+    @DisplayName("should save Long URL with key when Valid Url is passed")
+    @Test
+    void shortenUrlTest() throws UrlInvalidException {
+        LongUrlDto longurl = LongUrlDto.builder()
+                .longUrl("https://www.abc.com").build();
+        ReflectionTestUtils.setField(shortifyService, "validityInDays", 14);
+        when(shortifyRepository.save(Mockito.any(ShortUrl.class)))
+                .thenReturn(ShortUrl.builder()
+                        .longUrl(longurl.getLongUrl()).shortKey("8ad4840c")
+                        .createdDate(LocalDateTime.now()).build());
+        shortifyService.shortenUrl(longurl);
+        verify(shortifyRepository, times(1)).save(Mockito.any(ShortUrl.class));
+    }
+
+    @DisplayName("should throw UrlInvalidException when invalid Url is passed")
+    @Test
+    void shortenUrlTest2(){
+        LongUrlDto longurl = LongUrlDto.builder()
+                .longUrl("hghjjhk").build();
+        assertThrows(UrlInvalidException.class, () -> shortifyService.shortenUrl(longurl));
+
+    }
+
+
 
 }
