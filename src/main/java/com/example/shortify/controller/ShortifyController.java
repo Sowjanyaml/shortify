056ec756d9db0ce30d7be8c57fd25a435bbc1321
lakshmi <sowjanya.ml@gmail.com>
2023-03-19 13:22:10 +0100

    add open api documentation

--- src/main/java/com/example/shortify/controller/ShortifyController.java
+++ src/main/java/com/example/shortify/controller/ShortifyController.java
@@ -5,6 +5,12 @@ import com.example.shortify.dto.ShortUrlDto;
 import com.example.shortify.exception.UrlInvalidException;
 import com.example.shortify.exception.UrlNotFoundException;
 import com.example.shortify.service.ShortifyService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.media.Schema;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -16,14 +22,27 @@ public class ShortifyController {
     public ShortifyController(ShortifyService shortifyService) {
         this.shortifyService = shortifyService;
     }
-
+    @Operation(summary = "Shortens Long URL")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "returns key corresponding to the URL passed",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ShortUrlDto.class)) }),
+            @ApiResponse(responseCode = "400", description = "Invalid URL supplied",
+                    content = @Content) })
     @PostMapping()
     public ShortUrlDto shortenUrl(@RequestBody LongUrlDto url) throws UrlInvalidException {
         return shortifyService.shortenUrl(url);
     }
 
+    @Operation(summary = "Get a full url by its key")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Found the url corresponding to the key",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = String.class)) }),
+            @ApiResponse(responseCode = "404", description = "Key not found",
+                    content = @Content) })
     @GetMapping(value = "/{key}")
-    public String getUrl(@PathVariable String key) throws UrlNotFoundException {
+    public String getUrl(@Parameter(description = "key of the URL to be searched") @PathVariable String key) throws UrlNotFoundException {
         return shortifyService.getUrlByKey(key);
     }
 }
